447a19ced843a7885ac141d69514b226ef918333
Anthony Nguyen <anknguyen@gmail.com>
2014-03-08 00:10:30 +0000

    Added TMW player list scraping (no recording of data yet)

--- tmwstats.py
+++ tmwstats.py
@@ -1,25 +1,46 @@
 import os
 from flask import Flask
+import re
 import threading
-import time
+import urllib2
 
-threadTime = ""
+normalUsers = []
+GMs = []
 
 def create_app():
 	app = Flask(__name__)
+	
 	def writeTime():
-		global threadTime
+		global normalUsers
+		global GMs
+		
 		threading.Timer(60.00, writeTime).start()
-		threadTime = str(time.time())
+		
+		try:
+			req = urllib2.urlopen("http://server.themanaworld.org")
+			rawHTML = req.read()
+			
+			allUsers = re.findall(r'\<td\>(.+?)\</td\>', rawHTML)
+			numUsers = len(allUsers)
+			GMs = []
+			normalUsers = []
+			
+			for user in allUsers:
+				m = re.match(r'\<b\>(.+?)\<\/b\> \(GM\)', user)
+				if m is not None:
+					GMs.append(m.group(1))
+				else:
+					normalUsers.append(user)
+		except:
+			pass
+	
 	writeTime()
 	return app
 
 app = create_app()
 
 @app.route('/')
-def hello():
-	return 'Hello World!'
-
-@app.route("/times")
-def times():
-	return threadTime
+def main():
+	global GMs
+	global normalUsers
+	return "GMs:<br /> <b>{0}</b> <br /><br /><br />Players:<br />{1}".format("<br />".join(GMs), "<br />".join(normalUsers))
